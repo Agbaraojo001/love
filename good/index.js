2e2442162ad27b3f837417e7db64d5abe1731aea
agbaraojo001 <agbaraojoyusufabiodun@gmail.com>
2024-02-06 18:43:12 +0100

    version 3

---
+++ good/index.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('button')
+
+button.onclick = function () {
+    alert("Hellow Yusuf, how are you today?")
+
+}
+
+const btn = document.getElementById('btn')
+
+btn.onclick = () => {
+    alert("Can we meet later today for coffea and make neccessary plans on our business")
+}
